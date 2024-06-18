c1375319861e4a2b17d0d0b1a5ec2c0dc835dee6
HJW <86650128+Hwang-Jeong-Uk@users.noreply.github.com>
2024-06-18 10:46:22 +0900

    Title: 점수 비교, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240618/점수 비교/score-comparison.py
+++ 240618/점수 비교/score-comparison.py
@@ -1,9 +1,10 @@
-A1,A2=input().slpit()
-B1,B2=input().slpit()
+A1,A2=input().split()
+B1,B2=input().split()
 A1= int(A1)
 A2= int(A2)
 B1= int(B1)
 B2= int(B2)
+
 if A1>B1 and A2>B2:
     print(1)
 else:
