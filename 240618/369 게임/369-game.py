82e234f8b78b9709b710d818261da2d4e7a55d97
HJW <86650128+Hwang-Jeong-Uk@users.noreply.github.com>
2024-06-18 16:21:37 +0900

    Title: 369 게임, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240618/369 게임/369-game.py
+++ 240618/369 게임/369-game.py
@@ -1,7 +1,7 @@
 n=int(input())
 i=1
 while n>=i:
-    if i%3==0 or (i//10%3==0 and i>=10) :
+    if i%3==0 or (i//10%3==0 and i>=10) or (i%10%3==0 and i>=10) :
         print(0, end=" ")
     else:
         print(i,end=" ")
