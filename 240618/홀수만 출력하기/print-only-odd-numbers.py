f125a413fe495aeac8219bca17fbe6e388fa33a7
HJW <86650128+Hwang-Jeong-Uk@users.noreply.github.com>
2024-06-18 16:50:05 +0900

    Title: 홀수만 출력하기, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240618/홀수만 출력하기/print-only-odd-numbers.py
+++ 240618/홀수만 출력하기/print-only-odd-numbers.py
@@ -1,9 +1,10 @@
-N=input()
-i=0
-while i<len(N) :
-    e=int(N[i])
-    if e%2 != 0 and e%3==0:
-        print(e)
-    else:
-        pass
+N = int(input())
+
+i = 0
+# N개의 정수를 순서대로 입력받습니다.
+while i < N:
+    number = int(input())
+    # 입력된 수가 홀수이면서 3의 배수인지 확인합니다.
+    if number % 2 != 0 and number % 3 == 0:
+        print(number)
     i += 1
