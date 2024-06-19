08be294815985c9c483700d98747a3f12eb4454a
HJW <86650128+Hwang-Jeong-Uk@users.noreply.github.com>
2024-06-19 15:43:53 +0900

    Title: 몫 출력하기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240619/몫 출력하기/print-share.py
+++ 240619/몫 출력하기/print-share.py
@@ -2,10 +2,11 @@ cnt=0
 while True:
     n=int(input())
     if n%2!=0:
-        pass
+        continue
     elif n%2==0:
         c=n//2
         print(c)
-    cnt += 1
-    if cnt ==4 :
+        cnt += 1
+    if cnt == 3:
         break
+print(cnt)
