9ab6c08e7683c4adaf16210907599526379f0846
HJW <86650128+Hwang-Jeong-Uk@users.noreply.github.com>
2024-06-19 13:49:44 +0900

    Title: 완전수 판별기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240619/완전수 판별기/perfect-number-discriminator.py
+++ 240619/완전수 판별기/perfect-number-discriminator.py
@@ -1,11 +1,11 @@
 n=int(input())
 
 sum_val=0
-for i in range(1,(n)//2):
+for i in range(1,n+1):
     if n%i==0:
         sum_val += n//i+i
 
-sum_val=sum_val-n
+sum_val=(sum_val-2*n)/2
 if sum_val==n:
     print('P')
 else:
