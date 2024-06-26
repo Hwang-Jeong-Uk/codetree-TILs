86699adc4ed05b284d163927d058b0470d53153b
HJW <86650128+Hwang-Jeong-Uk@users.noreply.github.com>
2024-06-26 11:00:03 +0900

    Title: 알파벳 출력하기 2, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240626/알파벳 출력하기 2/print-alphabet-2.py
+++ 240626/알파벳 출력하기 2/print-alphabet-2.py
@@ -1,7 +1,10 @@
 N=int(input())
 cnt='A'
 for i in range(N):
+    for k in range(i):
+            print(" ",end=" ")
     for j in range(N-i):
+        
         print(cnt,end=" ")
         if cnt=='Z':
             cnt='A'
