d92a0138b8cfe5f0df3fd5765f721c99ba0b79fb
HJW <86650128+Hwang-Jeong-Uk@users.noreply.github.com>
2024-06-27 16:53:32 +0900

    Title: 연속부분수열일까, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240627/연속부분수열일까/contiguous-array-or-not.py
+++ 240627/연속부분수열일까/contiguous-array-or-not.py
@@ -4,15 +4,15 @@ B=list(map(int,input().split()))
 count=[]
 cnt=0
 
-for o,k in enumerate(B):
-    if k in A:
-        count.append(A.index(k))
+for i in range(n2):
+    if B[i] in A:
+        count.append(A.index(B[i]))
         
     else:
         pass
 for i in range(len(count)-1):
     cnt += (count[i+1]-count[i])
-    
+
 if cnt == (len(count)-1):
     print('Yes')
 else:
