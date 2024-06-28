b83b84aa8fed32308f1c35e67e22b929818c14c5
HJW <86650128+Hwang-Jeong-Uk@users.noreply.github.com>
2024-06-28 13:39:18 +0900

    Title: 500 근처의 수, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240628/500 근처의 수/near-500.py
@@ -0,0 +1,11 @@
+arr=list(map(int,input().split()))
+arrt=[]
+arrtt=[]
+for i in arr:
+    if i < 500:
+        arrt.append(i)
+    elif i >500:
+        arrtt.append(i)
+
+print(max(arrt),end=" ")
+print(max(arrtt))
